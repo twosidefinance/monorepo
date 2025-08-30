a86af8307ad93781a496090164b41c4b755aa6f8
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-08-30 23:55:28 +0530

    codebase monorepo init

---
+++ apps/solana/programs/solana/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Dua4QHV8oHr8Mxna9jngcTgACVVpitrAdDK4xVHufjCG");
+
+#[program]
+pub mod solana {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
