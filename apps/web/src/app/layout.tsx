97c8567eb3f566b832d7adad4b52e6219d2b8157
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-09-08 20:07:41 +0530

    feat(web): added color and text theme

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/src/styles/globals.css";
 import { Toaster } from "@buffcat/ui/components/sonner";
 import { CustomLayout } from "@/src/components/CustomLayout";
 
