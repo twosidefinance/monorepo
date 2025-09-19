a64f217c2923ba408ff1b543876cac86c74c8816
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-09-19 17:45:11 +0530

    feat(program: sol): written error codes

--- programs/solana/programs/solana/src/lib.rs
+++ programs/solana/programs/solana/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("Dua4QHV8oHr8Mxna9jngcTgACVVpitrAdDK4xVHufjCG");
 
 #[program]
-pub mod solana {
+pub mod Buffcat {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
@@ -14,3 +14,22 @@ pub mod solana {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+// Error Codes 
+#[error_code]
+pub enum BuffcatErrorCodes {
+    #[msg("Account not authorized.")]
+    NotAuthorized,
+    #[msg("Amount value sent is zero.")]
+    ZeroAmountValue,
+    #[msg("Address cannot be zero.")]
+    ZeroAddress,
+    #[msg("Amount value < Minimum amount.")]
+    InvalidAmount,
+    #[msg("Derivative not minted.")]
+    NoDerivativeDeployed,
+    #[msg("Derivative sent is not for this token.")]
+    InvalidDerivativeAddress,
+    #[msg("Token not whitelisted.")]
+    NotWhitelisted,
+}
