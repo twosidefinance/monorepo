c427a3e1c85ca77189476d701c088fb3bb0f5193
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-11-25 22:56:47 +0530

    refactor(web): set deployed contract addresses

--- apps/web/src/lib/envVariables.ts
+++ apps/web/src/lib/envVariables.ts
@@ -1,6 +1,6 @@
 export const envVariables = {
   twosideContract: {
-    eth: "0xA15BB66138824a1c7167f5E85b957d04Dd34E468",
-    base: "",
+    eth: "0xd81945CE1F5DF00418a9029f3a1c6aCd688f6E8a",
+    base: "0xd81945CE1F5DF00418a9029f3a1c6aCd688f6E8a",
   },
 };
